41c5f970e23f7d14debb0027fe9964f6a5ec413e
Anshul-Chaudhary98 <101334052+Anshul-Chaudhary98@users.noreply.github.com>
2022-06-26 13:27:05 +0530

    Add files via upload

---
+++ Factors.java
@@ -0,0 +1,21 @@
+package day5assignments;
+
+import java.util.Scanner;
+
+public class Factors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 0;
+		System.out.print("Enter the Number: ");
+		Scanner sc = new Scanner(System.in);
+		number = sc.nextInt();
+		System.out.print("Factors of "+ number + " are: ");
+		for(int i = 1; i <= number; i++) {
+			if(number % i == 0) {
+				System.out.print(" " + i);
+			}
+		}
+	}
+
+}
